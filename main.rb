9f8235268e5f7db2d66ae092ad0d28361b706f61
Renan Garcia <email@renangarcia.me>
2020-10-23 23:38:22 -0300

    create default route

--- main.rb
+++ main.rb
@@ -5,3 +5,7 @@ require_relative './controllers/favorites_controller'
 require_relative './controllers/manga_controller'
 require_relative './controllers/telegram_bot_controller'
 require_relative './controllers/bot_controller'
+
+get '/' do
+  ' ¯ \ _ (ツ) _ / ¯'
+end
